68668452c0c8a183eb39dcb20d680182bfa9eef3
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-08-01 13:25:38 +0200

    Member declarations

--- MultiOrder Type  Orderbook/src/Orderbook.cpp
+++ MultiOrder Type  Orderbook/src/Orderbook.cpp
@@ -65,8 +65,8 @@ public:
 		, orderId_{ orderId }
 		, side_{ side }
 		, price_{ price }
-		, initialQuanity_{ quantity }
-		, remainingQuantity { quantity }
+		, initialQuantity_{ quantity }
+		, remainingQuantity_ { quantity }
 	{ }
 	OrderId GetOrderId() const { return orderId_; }
 	Side GetSide() const { return side_; }
@@ -79,11 +79,16 @@ public:
 	{
 		if (quantity > GetRemainingQuantity())
 			throw std::logic_error(std::format("Order ({}) cannot be filled for more than its remaining quantity", GetOrderId()));
-		remainingQuantity -= quantity;
+		remainingQuantity_ -= quantity;
 	}
 
 private:
-
+	OrderType orderType_;
+	OrderId orderId_;
+	Side side_;
+	Price price_;
+	Quantity initialQuantity_;
+	Quantity remainingQuantity_;
 };
 
 int main()
