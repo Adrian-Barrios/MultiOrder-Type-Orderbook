d39f9cab06c7344d1c0152b7bb70e3fb39f255e2
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-08-01 13:03:50 +0200

    Add project files.

---
+++ MultiOrder Type  Orderbook/src/Orderbook.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <limits>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <unordered_map>
+#include <memory>
+#include <variant>
+#include <optional>
+#include <tuple>
+#include <format>
+
+enum class OrderType
+{
+	GoodTillCancel,
+	FillAndKill
+};
+
+enum class Side
+{
+	Buy,
+	Sell
+};
+
+using Price = std::int32_t;
+using Quantity = std::uint32_t;
+using OrderId = std::uint64_t;
+
+struct LevelInfo
+{
+	Price price_;
+	Quantity quantity_;
+};
+
+using levelInfos = std::vector<LevelInfo>;
+
+int main()
+{
+	return 0;
+}
