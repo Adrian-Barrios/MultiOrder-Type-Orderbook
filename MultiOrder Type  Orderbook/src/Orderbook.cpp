b0caff0fdeb045317f828d3435c7f360256358af
Adrian Barrios <adrianbarrioscortez@gmail.com>
2026-08-03 20:18:53 +0200

    Order matching logic.

--- MultiOrder Type  Orderbook/src/Orderbook.cpp
+++ MultiOrder Type  Orderbook/src/Orderbook.cpp
@@ -75,6 +75,7 @@ public:
 	Quantity GetIntitialQuantity() const { return initialQuantity_; }
 	Quantity GetRemainingQuantity() const { return remainingQuantity_; }
 	Quantity GetFilledQuantity() const { return GetIntitialQuantity() - GetRemainingQuantity(); }
+	bool isFilled() const { return GetRemainingQuantity() == 0; }
 	void Fill(Quantity quantity)
 	{
 		if (quantity > GetRemainingQuantity())
@@ -158,6 +159,75 @@ private:
 	std::map<Price, OrderPointers, std::less<Price>> asks_; // The best ask is th cheapest sell offer.
 	std::unordered_map<OrderId, OrderEntry> orders_;
 
+	bool CanMatch(Side side, Price price) const
+	{
+		if (side == Side::Buy)
+		{
+			if (asks_.empty())
+				return false;
+			const auto& [bestAsk, _] = *asks_.begin();
+			return price >= bestAsk;
+		}
+		else
+		{
+			if (bids_.empty())
+				return false;
+
+			const auto& [bestBid, _] = *bids_.begin();
+			return price <= bestBid;
+		}
+	}
+	Trades MatchOrders()
+	{
+		Trades trades;
+		trades.reserve(orders_.size());
+
+		while (true)
+		{
+			if (bids_.empty() || asks_.empty())
+				break;
+			auto& [bidPrice, bids] = *bids_.begin();
+			auto& [askPrice, asks] = *asks_.begin();
+
+			if (bidPrice < askPrice)
+				break;
+
+			while (bids.size() && asks.size())
+			{
+				auto& bid = bids.front();
+				auto& ask = asks.front();
+
+				Quantity quantity = std::min(bid->GetRemainingQuantity(), ask->GetRemainingQuantity());
+
+				bid->Fill(quantity);
+				ask->Fill(quantity);
+
+				if (bid->isFilled())
+				{
+					bids.pop_front();
+					orders_.erase(bid->GetOrderId());
+				}
+				if (ask->isFilled())
+				{
+					asks.pop_front();
+					orders_.erase(ask->GetOrderId());
+				}
+
+				if (bids.empty())
+					bids_.erase(bidPrice);
+
+				if (asks.empty())
+					asks_.erase(askPrice);
+
+				trades.push_back(Trade{
+					TradeInfo{ bid->GetOrderId(), bid->GetPrice(), quantity}
+					, TradeInfo {ask->GetOrderId(), ask->GetPrice(), quantity}
+					});
+
+
+			}
+		}
+	}
 };
 
 int main()
